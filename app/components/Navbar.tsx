14369f3af3de4d552eb0006a8b40cf7788b51a22
Eugene <eugyneehunter@gmail.com>
2025-12-03 00:18:39 +0300

    Update Navbar component to adjust margin and fix Sidebar border color

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div className="w-full fixed top-0 left-0 h-16 bg-gray-800 flex items-center px-4">
+        <div className="w-full ml-56 fixed top-0 left-0 h-16 bg-gray-800 flex items-center px-4">
             <p>This is a Navbar</p>
         </div>
     )
--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Sidebar = () => {
     return (
-        <div className="w-56 h-full border-r border-yellow-500 bg-gray-800 min-h-screen fixed top-0 text-white p-4">
-
+        <div className="w-56 h-full border-r border-yellow-600 bg-gray-800 min-h-screen fixed top-0 text-white p-4">
+            <p className='text-yellow-600 text-2xl text-center'>Tasker Sidebar</p>
         </div>
     )
 }
