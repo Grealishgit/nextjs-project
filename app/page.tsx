0b9f48c322d85974622aa8fa37a4f25eab93be33
Eugene <eugyneehunter@gmail.com>
2025-12-02 23:51:04 +0300

    Update background image in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center w-full min-h-screen"
       style={{
-        backgroundImage: "url('https://cdn.pixabay.com/photo/2025/11/13/17/30/common-nettle-9955222_1280.jpg')",
+        backgroundImage: "url('https://cdn.pixabay.com/photo/2025/10/02/15/52/mountains-9868748_1280.jpg')",
         backgroundSize: 'cover',
         backgroundPosition: 'center',
       }}
