a2aea99725884385efe32c1d0d5197f521acebbd
Eugene <eugyneehunter@gmail.com>
2025-12-03 00:15:29 +0300

    Add Sidebar component and update layout to include Navbar and Sidebar

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Sidebar = () => {
     return (
-        <div>Sidebar</div>
+        <div className="w-56 h-full border-r border-yellow-500 bg-gray-800 min-h-screen fixed top-0 text-white p-4">
+
+        </div>
     )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center w-full min-h-screen"
+    <div className="flex ml-56 flex-col items-center justify-center min-h-screen"
       style={{
         backgroundImage: "url('https://cdn.pixabay.com/photo/2025/10/02/15/52/mountains-9868748_1280.jpg')",
         backgroundSize: 'cover',
