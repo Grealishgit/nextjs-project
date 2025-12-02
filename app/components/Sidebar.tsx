72f10311cea9c1e6741c14a3608a30c646ef5689
Eugene <eugyneehunter@gmail.com>
2025-12-03 00:20:23 +0300

    Fix Sidebar text color and add Login and Register components

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Sidebar = () => {
     return (
         <div className="w-56 h-full border-r border-yellow-600 bg-gray-800 min-h-screen fixed top-0 text-white p-4">
-            <p className='text-yellow-600 text-2xl text-center'>Tasker Sidebar</p>
+            <p className='text-yellow-500 text-2xl text-center'>Tasker Sidebar</p>
         </div>
     )
 }
